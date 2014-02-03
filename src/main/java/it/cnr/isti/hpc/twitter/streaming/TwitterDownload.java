3616640f5d50aa220fba8f770cd9f83ac8c0970b
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-02-03 11:38:16 +0100

    add lifespan

--- src/main/java/it/cnr/isti/hpc/twitter/streaming/TwitterDownload.java
+++ src/main/java/it/cnr/isti/hpc/twitter/streaming/TwitterDownload.java
@@ -8,8 +8,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.zip.GZIPOutputStream;
 
 import org.slf4j.Logger;
@@ -28,6 +30,8 @@ import twitter4j.json.DataObjectFactory;
 
 class DateUtils {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(DateUtils.class);
 	/*
 	 * System.out.println(DateUtils.now("dd MMMMM yyyy"));
 	 * System.out.println(DateUtils.now("yyyyMMdd hh:mm"));
@@ -40,33 +44,41 @@ class DateUtils {
 	// public static String currentDate = "";
 
 	public static String now() {
-		
+
 		Calendar cal = Calendar.getInstance();
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
 		sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT"));
-		
-		//System.out.println(cal.ZONE_OFFSET);
-		
-		//System.out.println(sdf.format(cal.getTime()));
-		
-		return sdf.format(cal.getTime());
-		
 
+		// System.out.println(cal.ZONE_OFFSET);
+
+		// System.out.println(sdf.format(cal.getTime()));
+
+		return sdf.format(cal.getTime());
+
+	}
+
+	public static Date getDate(String date) {
+		SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+		try {
+			return sdf.parse(date);
+		} catch (ParseException e) {
+			logger.warn("parsing the date {} ", date);
+			return null;
+		}
 	}
 
 }
 
 public class TwitterDownload extends StatusAdapter {
 	private TwitterStream twitter;
-	private File f = null;
+	private File dumpDir = null;
 	private OutputStreamWriter writer;
 	private String currentDate = null;
-	
-	
-	ProjectProperties properties = new ProjectProperties(TwitterDownload.class);
-	private String rootfile = properties.get("twitter_dump");
+	private long fileLifespanInMillis;
 
+	ProjectProperties properties = new ProjectProperties(TwitterDownload.class);
+	private final String rootfile = properties.get("twitter_dump");
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(TwitterDownload.class);
@@ -81,14 +93,14 @@ public class TwitterDownload extends StatusAdapter {
 	}
 
 	public File makeDir(String dirName) {
-		 
+
 		// String rootfile = properties.getProperty("twitter_dump");
 		// String dir = properties.getProperty("twitter_dump_directory");
 
 		// put path
 
-		f = new File(dirName);
-		boolean success = f.mkdir();
+		dumpDir = new File(dirName);
+		boolean success = dumpDir.mkdir();
 		if (!success) {
 			System.out.println("Couldn't create directory!");
 			System.exit(-1);
@@ -96,53 +108,50 @@ public class TwitterDownload extends StatusAdapter {
 			System.out.println("Successfully created directory!");
 		}
 
-		return f;
+		return dumpDir;
 
 	}
 
 	public TwitterDownload(String dirName) {
 		super();
 		init(dirName);
-		
+		fileLifespanInMillis = (long) properties
+				.getInt("file.lifespan.in.days") * 24 * 60 * 60 * 1000;
+
 	}
-	
+
 	@Override
 	public void onStallWarning(StallWarning stallWarning) {
-	    System.out.println(stallWarning);
+		System.out.println(stallWarning);
 	}
-	
-	private void init(String dirName){
+
+	private void init(String dirName) {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setJSONStoreEnabled(true)
 				.setDebugEnabled(true)
-				.setOAuthConsumerKey(
-						properties.get("oauth.consumerKey"))
-				.setOAuthConsumerSecret(
-						properties.get("oauth.consumerSecret"))
-				.setOAuthAccessToken(
-						properties.get("oauth.accessToken"))
+				.setOAuthConsumerKey(properties.get("oauth.consumerKey"))
+				.setOAuthConsumerSecret(properties.get("oauth.consumerSecret"))
+				.setOAuthAccessToken(properties.get("oauth.accessToken"))
 				.setOAuthAccessTokenSecret(
 						properties.get("oauth.accessTokenSecret"))
 				.setGZIPEnabled(true);
-		
+
 		TwitterStreamFactory tf = new TwitterStreamFactory(cb.build());
 		twitter = tf.getInstance();
-		logger.info("authorization enabled: {}",twitter.getAuthorization().isEnabled());
+		logger.info("authorization enabled: {}", twitter.getAuthorization()
+				.isEnabled());
 
-		f = new File(dirName);
+		dumpDir = new File(dirName);
 
-		if (!f.exists()) {
-			f = makeDir(dirName);
+		if (!dumpDir.exists()) {
+			dumpDir = makeDir(dirName);
 		}
 	}
-	
-	
-	
 
 	public TwitterDownload() {
 		init(properties.get("twitter_dump_directory"));
 	}
-	
+
 	public void dumpSampleStream() {
 
 		StatusListener listener = new StatusListener() {
@@ -164,7 +173,7 @@ public class TwitterDownload extends StatusAdapter {
 
 			private void storeStatusJSON(String rawJSON) {
 				// store status json
-				
+
 				String d = DateUtils.now();
 
 				if (d.equals(currentDate)) {
@@ -182,6 +191,7 @@ public class TwitterDownload extends StatusAdapter {
 				// if i'm here then d != currentDate
 
 				// if i wrote something, i close the writer.
+
 				if (writer != null)
 					try {
 						writer.close();
@@ -193,11 +203,16 @@ public class TwitterDownload extends StatusAdapter {
 
 				currentDate = d;
 
-				String filename = f.getPath() + "/" + rootfile + d + ".txt.gz";
-				int i = 0; 
+				// remove file older than lifespan set in properties
+				removeOldDumpFiles();
+
+				String filename = dumpDir.getPath() + "/" + rootfile + d
+						+ ".txt.gz";
+				int i = 0;
 				File ff = new File(filename);
-				if (ff.exists()){
-					filename = f.getPath() + "/" + rootfile + d + "-"+i+".txt.gz";
+				if (ff.exists()) {
+					filename = dumpDir.getPath() + "/" + rootfile + d + "-" + i
+							+ ".txt.gz";
 					ff = new File(filename);
 					i++;
 				}
@@ -227,67 +242,57 @@ public class TwitterDownload extends StatusAdapter {
 					System.exit(-1);
 				}
 
-				// // current date NOW
-				// String d = DateUtils.now();
-				//
-				// // list of files in directory
-				//
-				// String[] files = f.();
-				//
-				// // if list is empty
-				// if (files.length == 0) {
-				// StringBuilder filename = new StringBuilder();
-				// filename.append(rootfile).append(d).append(".txt.gz");
-				// DateUtils.currentDate = d;
-				//
-				// FileOutputStream dest = new FileOutputStream(
-				// filename.toString());
-				// GZIPOutputStream compressedFile = new GZIPOutputStream(
-				// new BufferedOutputStream(dest));
-				// writer = new OutputStreamWriter(compressedFile, "UTF-8");
-				// writer.write(rawJSON);
-				// }
-				// // if list is not empty
-				// else {
-				// // list NOT empty, but the date is the SAME as the current
-				// // date file
-				// if (d == DateUtils.currentDate) {
-				// writer.write(rawJSON);
-				// }
-				//
-				// // list is NOT empty, but a NEW current date
-				// else {
-				// // close current file
-				// writer.close();
-				// DateUtils.currentDate = d;
-				//
-				// // create a new one with the new date
-				//
-				// StringBuilder filename = new StringBuilder();
-				// filename.append(rootfile).append(d).append(".txt.gz");
-				//
-				// FileOutputStream dest = new FileOutputStream(
-				// filename.toString());
-				// GZIPOutputStream compressedFile = new GZIPOutputStream(
-				// new BufferedOutputStream(dest));
-				// writer = new OutputStreamWriter(compressedFile, "UTF-8");
-				// writer.write(rawJSON);
-				// }
-				//
-				// }
+			}
 
+			private void removeOldDumpFiles() {
+				for (File f : dumpDir.listFiles()) {
+					if (isOld(f)) {
+						logger.info("removing file {}, lifespan expired",
+								f.getName());
+						f.delete();
+					}
+				}
+			}
+
+			private boolean isOld(File f) {
+				String name = f.getName();
+				String dt = name.substring(rootfile.length(),
+						rootfile.length() + 8);
+				Date filedate = DateUtils.getDate(dt);
+				Calendar cal = Calendar.getInstance();
+				long delta = (cal.getTime().getTime() - filedate.getTime());
+				return (delta > fileLifespanInMillis);
 			}
 
 			private void storeDeletionNoticeJSON(String rawJSON) {
 				// store status deletion notice json
 			}
 
-			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+			// @Override
+			// public void onTrackLimitationNotice(int numberOfLimitedStatuses)
+			// {
+			// }
+			//
+			// @Override
+			// public void onException(Exception ex) {
+			// ex.printStackTrace();
+			//
+			// }
+			//
+			// @Override
+			// public void onScrubGeo(long arg0, long arg1) {
+			// // TODO Auto-generated method stub
+			//
+			// }
+
+			public void onStallWarning(StallWarning arg0) {
+				// TODO Auto-generated method stub
+
 			}
 
-			public void onException(Exception ex) {
-				ex.printStackTrace();
-				
+			public void onException(Exception arg0) {
+				// TODO Auto-generated method stub
+
 			}
 
 			public void onScrubGeo(long arg0, long arg1) {
@@ -295,9 +300,9 @@ public class TwitterDownload extends StatusAdapter {
 
 			}
 
-			public void onStallWarning(StallWarning arg0) {
+			public void onTrackLimitationNotice(int arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 		};
@@ -309,5 +314,4 @@ public class TwitterDownload extends StatusAdapter {
 		twitter.sample();
 
 	}
-
 }
