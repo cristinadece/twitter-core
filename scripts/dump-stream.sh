53ddfe70f2543b9d61a067fb4e8d466e9c12f327
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-02-03 11:52:22 +0100

    output dir read from property file

--- scripts/dump-stream.sh
+++ scripts/dump-stream.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 
-EXPECTED_ARGS=1
+source scripts/config.sh
+
+
+EXPECTED_ARGS=0
 E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` dump_folder "
+  echo "Usage: `basename $0`"
   exit $E_BADARGS
 fi
 
-source scripts/config.sh
 
-DUMP=$1
 
-echo "Dumping the stream of the tweets in folder $DUMP (kill to finish)"
-$JAVA it.cnr.isti.hpc.twitter.streaming.TwitterDownloadCLI -input $DUMP
+echo "Dumping the stream of the tweets (kill to finish)"
+$JAVA it.cnr.isti.hpc.twitter.streaming.TwitterDownloadCLI
 
-echo "Stream dumped in $DUMP"
+echo "Stream dumped"
 
