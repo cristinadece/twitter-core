d9856374ae9360029231b723a0e592905473f564
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-02-03 12:11:42 +0100

    add html logfile

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,9 +4,11 @@ VERSION="0.0.1-SNAPSHOT"
 XMX="-Xmx2000m"
 #LOG=INFO
 PROJECT_NAME=twitter
-LOG=OFF
+LOG=INFO
 LOGAT=1000
 CLI=it.cnr.isti.hpc.twitter.cli
 E_BADARGS=65
+HTML_LOG=/data/twitter/logs/
 
-JAVA="java $XMX -Dlogat=$LOGAT -Dlog=$LOG -cp ./target/$PROJECT_NAME-$VERSION-jar-with-dependencies.jar "
+
+JAVA="java $XMX -Dlogfile=$HTML_LOG -Dlogat=$LOGAT -Dlog=$LOG -cp .:./target/$PROJECT_NAME-$VERSION-jar-with-dependencies.jar "
