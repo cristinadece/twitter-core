7d8d8fb6f26e9c45d97a7febc68e241260302529
Cristina Muntean <cristina.i.muntean@gmail.com>
2014-09-09 11:20:31 +0200

    makeBuckets.sh script tests if a file is newer than 7 days and split buckets only for those

--- makeBuckets.sh
+++ makeBuckets.sh
@@ -5,6 +5,10 @@ jar=./target/twitter-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
 for f in $FILES
 do
-	echo "Processing $f ...."
-	java -cp $jar it.cnr.isti.hpc.twitter.timebuckets.SplitFileInTimeBucketsCLI -input $f -output /Users/cris/Documents/workspace/analytics/buckets/ -interval 1800000
-done 
+	# we check if the file was modified less than 7 day ago, this way we don't calculate trends on very old files
+	if test `find $f -mtime -7`
+	then
+		echo "Processing $f"
+		#java -cp $jar it.cnr.isti.hpc.twitter.timebuckets.SplitFileInTimeBucketsCLI -input $f -output /Users/cris/Documents/workspace/analytics/buckets/ -interval 1800000
+	fi
+done
