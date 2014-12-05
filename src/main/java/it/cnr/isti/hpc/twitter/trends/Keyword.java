c7ef986abb52355dffeb8b6603cf7bd5c0913696
Cristina Muntean <cristina.i.muntean@gmail.com>
2014-12-05 10:33:47 +0100

    refactoring of fields in Keywords, added some new fiels: time and entities

--- src/main/java/it/cnr/isti/hpc/twitter/trends/Keyword.java
+++ src/main/java/it/cnr/isti/hpc/twitter/trends/Keyword.java
@@ -11,30 +11,53 @@ import java.util.List;
 
 public class Keyword {
 
-	private String name;
+	private String trend;
+	private String time;
 	// this is an item which holds the frequency of the keyword per bucket 
 	// (identified with timeInMillis)
-	private LinkedHashMap<String, Integer> bucketFrequency;
-	private List<String> tweetIds;
-	private List<String> userIds;
-	private List<String> first50tweets;
+	private transient LinkedHashMap<String, Integer> bucketFrequency;
+	private List<String> ids;
+	private List<String> users;
+	private List<String> tweets;
+	private List<String> entities;
 	private double zscore;
-	private double basefreq;
+	private double frequency;
 	
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public List<String> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(List<String> entities) {
+		this.entities = entities;
+	}
 	
+	public void addEntity(String entity){
+		
+		if (!this.entities.contains(entity)){
+			this.entities.add(entity);
+		}
+	}
 	
 	public List<String> getFirst50tweets() {
-		return first50tweets;
+		return tweets;
 	}
 
 	public void setFirst50tweets(List<String> first50tweets) {
-		this.first50tweets = first50tweets;
+		this.tweets = first50tweets;
 	}
 	
 	public void addFirst50tweets(String tweet){
-		if (this.first50tweets.size()<=50){
-			if (!this.first50tweets.contains(tweet)){
-				this.first50tweets.add(tweet);
+		if (this.tweets.size()<=50){
+			if (!this.tweets.contains(tweet)){
+				this.tweets.add(tweet);
 			}
 		}
 	}
@@ -49,19 +72,19 @@ public class Keyword {
 	}
 
 	public double getBasefreq() {
-		return basefreq;
+		return frequency;
 	}
 
 	public void setBasefreq(double basefreq) {
-		this.basefreq = basefreq;
+		this.frequency = basefreq;
 	}
 
-	public String getName() {
-		return name;
+	public String getTrend() {
+		return trend;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setTrend(String trend) {
+		this.trend = trend;
 	}
 	
 	public LinkedHashMap<String, Integer> getBucketFrequency() {
@@ -84,62 +107,62 @@ public class Keyword {
 	}
 
 	public List<String> getTweetIds() {
-		return tweetIds;
+		return ids;
 	}
 
 	public void setTweetIds(List<String> tweetIds) {
-		this.tweetIds = tweetIds;
+		this.ids = tweetIds;
 	}
 	
 	public void addTweetId(String tweetId){
-		if (!this.tweetIds.contains(tweetId)){
-			this.tweetIds.add(tweetId);
+		if (!this.ids.contains(tweetId)){
+			this.ids.add(tweetId);
 		}
 	}
 
 	public List<String> getUserIds() {
-		return userIds;
+		return users;
 	}
 
 	public void setUserIds(List<String> userIds) {
-		this.userIds = userIds;
+		this.users = userIds;
 	}
 	
 	public void addUserId(String userId){
-		if (!this.userIds.contains(userId)){
-			this.userIds.add(userId);
+		if (!this.users.contains(userId)){
+			this.users.add(userId);
 		}
 	}
 
 	public Keyword() {
 		bucketFrequency = new LinkedHashMap<String, Integer>();
-		tweetIds = new ArrayList<String>();
-		userIds = new ArrayList<String>();
-		first50tweets = new ArrayList<String>();
+		ids = new ArrayList<String>();
+		users = new ArrayList<String>();
+		tweets = new ArrayList<String>();
 	}
 	
 	public Keyword(String word) {
-		name = word;
+		trend = word;
 		//init
 		bucketFrequency = new LinkedHashMap<String, Integer>();
-		tweetIds = new ArrayList<String>();
-		userIds = new ArrayList<String>();
-		first50tweets = new ArrayList<String>();
+		ids = new ArrayList<String>();
+		users = new ArrayList<String>();
+		tweets = new ArrayList<String>();
 	}
 	
 	public Keyword(String word, String interval, String tweetId, String userID, String tweet) {
 		
 		//init
 		bucketFrequency = new LinkedHashMap<String, Integer>();
-		tweetIds = new ArrayList<String>();
-		userIds = new ArrayList<String>();
-		first50tweets = new ArrayList<String>();
+		ids = new ArrayList<String>();
+		users = new ArrayList<String>();
+		tweets = new ArrayList<String>();
 		
-		name = word;
+		trend = word;
 		bucketFrequency.put(interval, 1);
-		tweetIds.add(tweetId);
-		userIds.add(userID);
-		first50tweets.add(tweet);
+		ids.add(tweetId);
+		users.add(userID);
+		tweets.add(tweet);
 	}
 
 	public static void main(String[] args) {
